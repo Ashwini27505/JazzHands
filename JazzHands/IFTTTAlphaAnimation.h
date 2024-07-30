3563c8f5ca09cb999d20b50e29fb395fd6b68f18
Ashwini27505 <ashwinigundale2003@gmail.com>
2024-07-30 18:39:30 +0530

    Update IFTTTAlphaAnimation.h

--- JazzHands/IFTTTAlphaAnimation.h
+++ JazzHands/IFTTTAlphaAnimation.h
@@ -8,7 +8,7 @@
 
 #import "IFTTTViewAnimation.h"
 
-@interface IFTTTAlphaAnimation : IFTTTViewAnimation <IFTTTAnimatable>
+
 
 - (void)addKeyframeForTime:(CGFloat)time alpha:(CGFloat)alpha;
 - (void)addKeyframeForTime:(CGFloat)time alpha:(CGFloat)alpha withEasingFunction:(IFTTTEasingFunction)easingFunction;
